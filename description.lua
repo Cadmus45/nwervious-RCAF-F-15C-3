c494b2f3938795b407a65bcd3d8dd351fe7abce0
Cadmus45 <59544303+Cadmus45@users.noreply.github.com>
2021-07-03 22:10:36 -0700

    Add files via upload

---
+++ description.lua
@@ -0,0 +1,33 @@
+livery = {
+	{"f15nose", 0 ,"f15_nose",false};
+	{"f15nose", 3 ,"f15_decol_empty",true};
+	{"f15bottom", 0 ,"f15_bottom",false};
+	{"f15bottom", 2 ,"../JASDF 62-8870/f15_bottom_spec",false}; --linked
+	{"f15bottom", 3 ,"f15_decol_empty",true};
+	{"f15centr", 0 ,"f15_centr",false};
+	{"f15centr", 2 ,"../JASDF 62-8870/f15_centr_spec",false}; --linked
+	{"f15wingl", 0 ,"f15_wing_l",false};
+	{"f15wingl", 3 ,"f15_decol_empty",true};
+	{"f15wingr", 0 ,"f15_wing_r",false};
+	{"f15stab", 0 ,"f15_stab_v",false};
+	{"f15stab", 3 ,"f15_decol_empty",true};
+	--{"f15numbers", 0 ,"f15_numbers",false};
+	{"f15numbers", 0 ,"empty",true};
+	{"pilot_F15_patch", 0 ,"f15_decol_empty",true};
+	--{"pilot_F15_00_body", 0 ,"pilot_f15_00_a",true};
+	{"pilot_F15_00_body", 0 ,"../JASDF 62-8870/pilot",false}; --linked
+	{"f15PTB", 0 ,"../JASDF 62-8870/f15_PTB",false}; --linked
+	{"f15PTB", 2 ,"../JASDF 62-8870/f15_ptb_spec",false}; --linked
+	{"f15_aces2", 0 ,"eject",false};
+	{"f15_cockpit", 0 ,"../JASDF 62-8870/cockpit",false}; --linked
+	
+	
+	
+	
+
+
+}
+
+--------------------------------------------------
+name = "nwervious RCAF livery"
+countries = {"USA", "C"}
